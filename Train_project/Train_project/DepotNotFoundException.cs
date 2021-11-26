bfc7caa091243cdaee641db6f54ba1746b11f2a6
Арутюнян Дмитрий <Retr073@yandex.ru>
2021-11-26 22:04:57 +0400

    Арутюнян Д. ПИбд-22 Лабораторная 7 (База) готова

---
+++ Train_project/Train_project/DepotNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train_project
+{
+    /// <summary>
+    /// Класс-ошибка "Если не найден поезд по определенному месту"
+    /// </summary>
+    class DepotNotFoundException : Exception
+    {
+        public DepotNotFoundException(int i) : base("Не найден поезд по месту " + i)
+        { }
+    }
+}
